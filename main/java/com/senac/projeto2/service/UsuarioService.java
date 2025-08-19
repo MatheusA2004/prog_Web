9f5ef526db18337711276594d9b719749a3df30a
Matheus <matheusa026@gmail.com>
2025-08-18 21:15:27 -0300

    criação de colunas

---
+++ main/java/com/senac/projeto2/service/UsuarioService.java
@@ -0,0 +1,15 @@
+package com.senac.projeto2.service;
+
+import com.senac.projeto2.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+
+}
